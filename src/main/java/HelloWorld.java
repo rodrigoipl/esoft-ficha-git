7484454d670da17cbfc522375f5ff30ef1c483e4
Joel Matos <2191260@my.ipleiria.pt>
2022-03-02 10:18:49 +0000

    adicionar o meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,9 +2,12 @@ import javax.swing.*;
 
 public class HelloWorld extends JFrame {
     private JPanel panel1;
+    private  JLabel lblMensagem2;
+
 
     public HelloWorld(){
         setContentPane(panel1);
+        lblMensagem2.setText("O Joel esteve aqui");
         pack();
     }
 
