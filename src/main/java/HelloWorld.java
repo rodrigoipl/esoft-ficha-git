a2e921696b797db910d30209956b3a3fde3ca574
Rodrigo Sousa <2191243@my.ipleiria.pt>
2022-03-02 10:27:18 +0000

    adicionar o meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,8 +2,11 @@ import javax.swing.*;
 
 public class HelloWorld extends JFrame {
     private JPanel panel1;
+    private JLabel lblMensagem;
+    private JLabel lblMensagem2;
 
     public HelloWorld(){
+        lblMensagem2.setText("O Rodrigo Sousa esteve aqui");
         setContentPane(panel1);
         pack();
     }
